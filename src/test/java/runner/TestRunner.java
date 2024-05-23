455f15212b3132ef059fa47af43b4e48ec9c7263
divakar9819 <vermadivakar10051998@gmail.com>
2024-05-23 15:01:02 +0530

    modification in pom file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "stepDef",
+        plugin = {"json:target/cucumber-report.json", "pretty", "html:target/test-report.html"},
         publish = true,
         monochrome = true
 )
